3b919f30c226a08421a77187c43808d173ddac3d
Anni-Mishra <mishraajinkya24@gmail.com>
2025-06-25 16:38:53 +0530

    created LOGOUT & EDIT profile APIs

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,19 +1,37 @@
 const validator = require("validator");
 
 const validateSignUpData = (req) => {
-    const { firstName, lastName, emailId, password } = req.body;
+  const { firstName, lastName, emailId, password } = req.body;
 
-    if(!firstName || !lastName) {
-        throw new Error ("Name is not valid... ");
-    }
-    else if(!validator.isEmail(emailId)){
-        throw new Error ("Email is not valid... ");
-    }
-    else if(!validator.isStrongPassword(password)){
-        throw new Error ("Password is not strong... ");
-    }
-}
+  if (!firstName || !lastName) {
+    throw new Error("Name is not valid... ");
+  } else if (!validator.isEmail(emailId)) {
+    throw new Error("Email is not valid... ");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("Password is not strong... ");
+  }
+};
+
+const validateEditProfileData = (req) => {
+  const allowedEditFields = [
+    "firstName",
+    "lastName",
+    "emailId",
+    "photoURL",
+    "gender",
+    "age",
+    "about",
+    "skills",
+  ];
+
+  const isEditAllowed = Object.keys(req.body).every((field) => 
+    allowedEditFields.includes(field)
+  );
+
+  return isEditAllowed;
+};
 
 module.exports = {
-    validateSignUpData,
-}
+  validateSignUpData,
+  validateEditProfileData,
+};
